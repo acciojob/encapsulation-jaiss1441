f7d28fd6e97360a1cccb911789538005669b7349
jais1441 <jaiswalvikas7276@gmail.com>
2024-06-25 22:18:15 +0530

    Task Commplete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly rw = new RWOnly();
+//        rw.name = "vikas";
+//        System.out.print(rw.name);
+        //java: name has private access in RWOnly
+        System.out.println(rw.getName());
+        rw.setName("Jaiswal");
+        System.out.println(rw.getName());
+
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name = "Vikas";
+
+
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String value){
+        name = value;
+    }
+
+
 }
